08c2485bc2db0ba611c80d3f469d5b4468f4e16f
Don Cooper <don_cooper@hotmail.com>
2017-06-07 17:00:26 -0400

    New and create methods done

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -8,4 +8,21 @@ class MemesController < ApplicationController
     @meme = Meme.find(params[:id])
   end
 
+  def new
+    @meme = Meme.new
+  end
+
+  def create
+    @meme = Meme.new(picture_params)
+    if @meme.save
+      redirect_to "/memes"
+    else
+      render :new
+    end
+  end
+
+  def picture_params
+    { title: params[:meme][:title], url: params[:meme][:url] }
+  end
+
 end
