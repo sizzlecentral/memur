3888201d9b40bf526af09d15edf4e3195b4aea7a
Don Cooper <don_cooper@hotmail.com>
2017-06-20 06:58:39 -0400

    Bcrpyt added, User model with attributes created

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607203054) do
+ActiveRecord::Schema.define(version: 20170620105736) do
 
   create_table "memes", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
