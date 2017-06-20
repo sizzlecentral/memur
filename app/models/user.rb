585c096a3955548757e65f2370dfe008ec3dd1b6
Don Cooper <don_cooper@hotmail.com>
2017-06-20 07:29:23 -0400

    Add new user working, flash alert showing on home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :email, :password, :password_confirmation, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'users/new'
+  get 'users/create'
+  resources :users
+
   root 'memes#index'
   resources :memes
+
 end
